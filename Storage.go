cc36f273e931e13a26f06ddafd7ad925086ff495
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-01-15 23:03:28 +0200

    Storage struct

--- Storage.go
+++ Storage.go
@@ -10,3 +10,29 @@ type StorageInterface interface {
 type Storage struct {
 	redis redis.UniversalClient
 }
+
+type DisciplineWithScores struct {
+	id          int
+	name        string
+	scoreRating DisciplineScoreRating
+	scores      []Score `json:,omitempty`
+}
+
+type DisciplineScoreRating struct {
+	total         float32
+	minTotal      float32
+	maxTotal      float32
+	rating        int
+	studentsCount int
+}
+
+type Score struct {
+	lesson struct {
+		id     int
+		date   string
+		typeId int
+	}
+	lessonHalf uint
+	score      float32
+	isAbsent   bool
+}
