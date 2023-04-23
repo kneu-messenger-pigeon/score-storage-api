0ae60ff08f043bf53f41692e22f8061ba400fd22
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-04-23 17:23:41 +0300

    Switch to use Error Response

--- ApiController.go
+++ ApiController.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	scoreApi "github.com/kneu-messenger-pigeon/score-api"
 	"io"
 	"net/http"
 	"strconv"
@@ -15,13 +16,17 @@ type ApiController struct {
 func (controller *ApiController) getStudentDisciplines(c *gin.Context) {
 	studentId, _ := strconv.Atoi(c.Param("student_id"))
 	if studentId <= 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect student_id: " + c.Param("student_id")})
+		c.JSON(http.StatusBadRequest, scoreApi.ErrorResponse{
+			Error: "Incorrect student_id: " + c.Param("student_id"),
+		})
 
 	} else {
 		disciplineScoreResults, err := controller.storage.getDisciplineScoreResultsByStudentId(studentId)
 
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, scoreApi.ErrorResponse{
+				Error: err.Error(),
+			})
 
 		} else {
 			c.JSON(http.StatusOK, disciplineScoreResults)
@@ -34,18 +39,26 @@ func (controller *ApiController) getStudentDiscipline(c *gin.Context) {
 	disciplineId, _ := strconv.Atoi(c.Param("discipline_id"))
 
 	if studentId <= 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect student_id: " + c.Param("student_id")})
+		c.JSON(http.StatusBadRequest, scoreApi.ErrorResponse{
+			Error: "Incorrect student_id: " + c.Param("student_id"),
+		})
 	} else if disciplineId <= 0 {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Incorrect discipline_Id: " + c.Param("disciplineId")})
+		c.JSON(http.StatusBadRequest, scoreApi.ErrorResponse{
+			Error: "Incorrect discipline_Id: " + c.Param("disciplineId"),
+		})
 
 	} else {
 		disciplineScoreResult, err := controller.storage.getDisciplineScoreResultByStudentId(studentId, disciplineId)
 
 		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			c.JSON(http.StatusInternalServerError, scoreApi.ErrorResponse{
+				Error: err.Error(),
+			})
 
 		} else if disciplineScoreResult.Discipline.Id == 0 {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Discipline not exists: " + c.Param("disciplineId")})
+			c.JSON(http.StatusNotFound, scoreApi.ErrorResponse{
+				Error: "Discipline not exists: " + c.Param("disciplineId"),
+			})
 
 		} else {
 			c.JSON(http.StatusOK, disciplineScoreResult)
