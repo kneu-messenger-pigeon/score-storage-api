5d5bfe48007592d10fb38be5f343d11e4c4ee86c
Anton Berezhnyi <aberezhniy@clips4sale.com>
2023-07-07 15:52:28 +0300

    Ignore main.go

---
+++ app.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v9"
+	"io"
+	"net/http"
+	"os"
+)
+
+const ExitCodeMainError = 1
+
+func runApp(out io.Writer, listenAndServe func(string, http.Handler) error) error {
+	envFilename := ""
+	if _, err := os.Stat(".env"); err == nil {
+		envFilename = ".env"
+	}
+
+	var opt *redis.Options
+	config, err := loadConfig(envFilename)
+	if err == nil {
+		opt, err = redis.ParseURL(config.redisDsn)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	redis := redis.NewClient(opt)
+
+	_, err = redis.Ping(context.Background()).Result()
+	if err != nil {
+		fmt.Fprintf(out, "Failed to connect to redis: %s\n", err.Error())
+	}
+
+	storage := NewStorage(redis, context.Background())
+
+	gin.SetMode(gin.ReleaseMode)
+	return listenAndServe(
+		config.listenAddress,
+		setupRouter(out, storage),
+	)
+}
+
+func handleExitError(errStream io.Writer, err error) int {
+	if err != nil {
+		_, _ = fmt.Fprintln(errStream, err)
+	}
+
+	if err != nil {
+		return ExitCodeMainError
+	}
+
+	return 0
+}
--- main.go
+++ main.go
@@ -1,65 +1,12 @@
+//go:build !test
+
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v9"
-	"io"
 	"net/http"
 	"os"
 )
 
-const ExitCodeMainError = 1
-
-type httpInterface interface {
-	ListenAndServe(addr string, handler http.Handler) error
-}
-
 func main() {
 	os.Exit(handleExitError(os.Stderr, runApp(os.Stdout, http.ListenAndServe)))
-}
-
-func runApp(out io.Writer, listenAndServe func(string, http.Handler) error) error {
-	envFilename := ""
-	if _, err := os.Stat(".env"); err == nil {
-		envFilename = ".env"
-	}
-
-	var opt *redis.Options
-	config, err := loadConfig(envFilename)
-	if err == nil {
-		opt, err = redis.ParseURL(config.redisDsn)
-	}
-
-	if err != nil {
-		return err
-	}
-
-	redis := redis.NewClient(opt)
-
-	_, err = redis.Ping(context.Background()).Result()
-	if err != nil {
-		fmt.Fprintf(out, "Failed to connect to redis: %s\n", err.Error())
-	}
-
-	storage := NewStorage(redis, context.Background())
-
-	gin.SetMode(gin.ReleaseMode)
-	return listenAndServe(
-		config.listenAddress,
-		setupRouter(out, storage),
-	)
-}
-
-func handleExitError(errStream io.Writer, err error) int {
-	if err != nil {
-		_, _ = fmt.Fprintln(errStream, err)
-	}
-
-	if err != nil {
-		return ExitCodeMainError
-	}
-
-	return 0
 }
